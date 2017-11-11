01e60bcd1f2f3b6d7e14c9e78cc4f895075f61bd
Owain Lewis <owain@owainlewis.com>
2017-11-11 09:37:53 +0000

    Update structure

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,6 @@
 module Main where
 
-import           Language.Scheme.Internal.AST
-import           Language.Scheme.Internal.Core
-import           Language.Scheme.Internal.Eval
-import           Language.Scheme.Internal.Parser
-
-import qualified Data.Text.IO                    as TIO
-
--- | Given a file path, read the contents and evaulate it
---
-runSchemeFile :: FilePath -> IO Scheme
-runSchemeFile filePath = do
-  contents <- TIO.readFile filePath
-  runSchemeWithDefaultEnv (evalSchemeText contents)
+import qualified Language.Scheme.Scheme as Scm
 
 main :: IO ()
-main = runSchemeFile "scratch.scm" >>= print
+main = Scm.executeFile "scratch.scm" >>= print
--- src/Language/Scheme/Scheme.hs
+++ src/Language/Scheme/Scheme.hs
@@ -1,6 +1,17 @@
-module Language.Scheme.Scheme where
+module Language.Scheme.Scheme
+  ( executeFile)
+  where
 
 import           Language.Scheme.Internal.AST
 import           Language.Scheme.Internal.Core
 import           Language.Scheme.Internal.Eval
 import           Language.Scheme.Internal.Parser
+
+import qualified Data.Text.IO                    as TIO
+
+-- | Given a file path, read the contents and evaulate it
+--
+executeFile :: FilePath -> IO Scheme
+executeFile filePath = do
+  contents <- TIO.readFile filePath
+  runSchemeWithDefaultEnv (evalSchemeText contents)
