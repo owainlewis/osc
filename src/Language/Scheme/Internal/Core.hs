c65df1a2033a490cba4faa64a8ebfc7431b7fb9e
Owain <owain@owainlewis.com>
2017-11-21 21:26:44 +0000

    Updates for eval

--- src/Language/Scheme/Internal/Core.hs
+++ src/Language/Scheme/Internal/Core.hs
@@ -79,15 +79,16 @@ binopFold op seed args = case args of
 
 -------------------------------------------------------------
 
-numOp op (Number x) (Number y) = return $ Number $ op x  y
-numOp op Nil        (Number y) = return $ Number y
-numOp op (Number x) Nil        = return $ Number x
-numOp op x           y         = throw $ TypeException "must be numeric"
+numOp op (Number x) (Number y) = return $ Number $ op x y
+numOp op x           y         = throw $ TypeException "expecting two numeric arguments"
+
+stringOp op (String x) (String y) = return $ Number $ op x y
+stringOp op x           y         = throw $ TypeException "expecting two string arguments"
 
 eq :: Scheme -> Scheme -> Eval Scheme
 eq (Atom   x) (Atom   y) = return . Bool $ x == y
 eq (Number x) (Number y) = return . Bool $ x == y
 eq (String x) (String y) = return . Bool $ x == y
 eq (Bool   x) (Bool   y) = return . Bool $ x == y
-eq Nil        Nil       = return $ Bool True
+eq (Nil)      (Nil)      = return $ Bool True
 eq  _          _         = return $ Bool False
--- src/Language/Scheme/Internal/Eval.hs
+++ src/Language/Scheme/Internal/Eval.hs
@@ -15,13 +15,32 @@ import           Control.Monad.Reader
 import qualified Data.Map                        as Map
 import qualified Data.Text                       as T
 import qualified Language.Scheme.Internal.Parser as P
+import Data.Semigroup((<>))
+
+-- Tracer function for debugging
+debugEval :: Eval Scheme
+debugEval = do
+  env <- ask
+  liftIO . print $ env
+  return Nil
+
+pairMap :: (a -> a) -> (a -> a) -> [a] -> [(a,a)]
+pairMap _ _ [] = []
+pairMap f g (x:y:xs) = (f x, g y) : pairMap f g xs
+pairMap f g (x:xs) = pairMap f g xs
+
+unwindLetForms :: [Scheme] -> Eval [(T.Text, Scheme)]
+unwindLetForms [] = return []
+unwindLetForms ((Atom x):y:xs) = do
+  y' <- eval y
+  return [(x, y')]
 
 eval :: Scheme -> Eval Scheme
 eval (Number i)                 = return (Number i)
 eval (String s)                 = return (String s)
 eval (Bool b)                   = return (Bool b)
 eval (List [])                  = return Nil
-eval Nil                        = return Nil
+eval (Nil)                      = return Nil
 eval (Atom a)                   = getVar a
 -- Quoted values
 eval (List [Atom "quote", val]) = return val
@@ -35,6 +54,12 @@ eval (List [Atom "if", predicate, t, f]) = do
 -- This state represents simple function application.
 -- We first lookup the first argument (f xs) and then apply
 -- the evaluated inner fn to the evaluated args
+
+eval (List [Atom "let", (List boundVars), expr]) = do
+  env <- ask
+  local (const (Map.fromList [] <> env))
+  debugEval
+
 eval (List (x:xs)) = do
   env    <- ask
   -- The scheme function to apply
@@ -59,7 +84,7 @@ setEnv :: T.Text -> Scheme -> Eval T.Text
 setEnv name expr = do
     env     <- ask
     evalVal <- eval expr
-    local (const $ Map.insert name  evalVal env) (pure name)
+    local (const $ Map.insert name evalVal env) (pure name)
 
 --------------------------------------------------------
 
@@ -74,5 +99,10 @@ runScheme expr env = runReaderT (unEval expr) env
 runSchemeWithDefaultEnv :: Eval a -> IO a
 runSchemeWithDefaultEnv = flip runScheme defaultEnv
 
+----------------------------------------------------------
+
 doExpr :: EnvCtx -> T.Text -> IO Scheme
 doExpr env expr = runScheme (evalSchemeText expr) env
+
+debug :: String -> IO Scheme
+debug = doExpr defaultEnv . T.pack
